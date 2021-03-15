9dbe4e7de791ff7d15cdaccd667217a4aec7e0fe
Wellan412 <80246610+Wellan412@users.noreply.github.com>
2021-03-15 11:49:17 +0100

    tp3 questions 1 et 2

--- TP3/SitePro/template_footer.php
+++
@@ -1,3 +0,0 @@
-<div class="pied">
-        <footer>Merci de votre visite, n'hésitez pas à revenir me voir !<br> See you soon !</footer>
-    </div>
--- TP3/question1/connected.php
+++ TP3/question1/connected.php
@@ -1,10 +1,12 @@
 <?php
 // on simule une base de données
 $users = array(
-// login => password
+    // login => password
     'riri' => 'fifi',
     'yoda' => 'maitrejedi',
-    'Wellan_412' => 'Hugo1998'   );
+    'Wellan_412' => 'blabla' );
+
+
 
 $login = "anonymous";
 $errorText = "";
@@ -13,25 +15,18 @@ $successfullyLogged = false;
 if(isset($_GET['login']) && isset($_GET['password'])) {
     $tryLogin=$_GET['login'];
     $tryPwd=$_GET['password'];
-
+}
 // si login existe et password correspond
 if( array_key_exists($tryLogin,$users) && $users[$tryLogin]==$tryPwd ) {
     $successfullyLogged = true;
     $login = $tryLogin;
-} else
+} else{
     $errorText = "Erreur de login/password";
-} else
-    $errorText = "Merci d'utiliser le formulaire de login";
+}
 
 if(!$successfullyLogged) {
     echo $errorText;
 } else {
-    echo "<h1>Bienvenu ".$login."</h1>";
-}
-
-if($successfullyLogged){
-    require_once('index.php');
-}else{
-
+echo "<h1>Bienvenu ".$login."</h1>";
 }
 ?>
