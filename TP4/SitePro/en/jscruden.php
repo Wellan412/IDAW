70be5146ec89c5473bf79ee09d89330c019839fe
Wellan412 <80246610+Wellan412@users.noreply.github.com>
2021-03-16 12:11:47 +0100

    avancement TP4

---
+++ TP4/SitePro/en/jscruden.php
@@ -0,0 +1,33 @@
+<!-- <form onsubmit="onFormSubmit();" autocomplete="off"> -->
+    <table>
+        <tr>
+            <th>Nom</th>
+            <td><input type="text" name="Nom"></td>
+        </tr>
+        <tr>
+            <th>Prénom</th>
+            <td><input type="text" name="prénom"></td>
+        </tr>
+        <tr>
+            <th>Prénom</th>
+            <td><input type="text" name="date" placeholder="dd/mm/YYYY"></td>
+        </tr>
+        <tr>
+    
+            <select name="cours" id="adore">
+                <option value="">Adore le cours ?</option>
+        </tr>
+       
+        </table>
+</form>
+
+
+
+
+<!-- <script>
+    function onFormSubmit() {
+    // prevent the form to be sent to the server
+    event.preventDefault();
+   
+    }
+</script> -->
---
+++ TP4/SitePro/fr/jscrudfr.php
@@ -0,0 +1,39 @@
+<!-- <form onsubmit="onFormSubmit();" autocomplete="off"> -->
+    <table>
+        <tr>
+            <th>Nom</th>
+            <td><input type="text" id="nom" name="Nom"></td>
+        </tr>
+        <tr>
+            <th>Prénom</th>
+            <td><input type="text" id="prenom" name="prenom"></td>
+        </tr>
+        <tr>
+            <th>Prénom</th>
+            <td><input type="text" id="date" name="date" placeholder="dd/mm/YYYY"></td>
+        </tr>
+        <tr>
+            <th>Adore le cours</th>
+
+            <td><input type="checkbox" id="cours" name="adorecours"></td>
+  
+        </tr>
+        <tr>
+            <th>Remarques</th>
+
+            <td><input type="text" id="rmp" name="remarques"></td>
+  
+        </tr>       
+        </table>
+</form>
+
+
+
+
+<!-- <script>
+    function onFormSubmit() {
+    // prevent the form to be sent to the server
+    event.preventDefault();
+   
+    }
+</script> -->
